f727c3d16580142f0a2bdbff6993ba6cd05f5445
JaskierBared <mateuszswiderski009@gmail.com>
2023-06-21 21:30:16 +0200

    Added components to view items in equipment

--- src/components/Equipment/Equipment.tsx
+++ src/components/Equipment/Equipment.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Equipment.css'
 import { EquipmentCell } from './EquipmentCell';
+import { getArmors, getWeapons } from '../../functions/getEquipment';
 
-const ceils = ['', '', '', '', '', '', '', '', '', '', '','', '', '', '', '', '', '', '', '', '', '','', '', '', '', '', '', '', '',]
+const ceils = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22','23', '24', '25', '26', '27', '28', '29', '30',]
 
 
 export const Equipment = () => {
+  const [armor, setArmor] = useState<any>([
+    { id: '1', name: 'error' ,quantity: 1, img: 'skorzany_pancerz', picture: 'error', needPicture:'error'},
+    { id: '2', name: 'error' ,quantity: 1, img: 'farmers_clothing_icon', picture: 'error', needPicture:'error'},
+
+  ]);
+  const [weapon, setWeapon] = useState<any>([])
+
+
+
+  useEffect(() => {
+    const AsyncFunction  = async () => {
+    setWeapon(await getWeapons())
+    setArmor(await getArmors())
+  
+  AsyncFunction()
+    }
+},[]);
 
   return (
     <div className='eq'>
-        {ceils.map(() => (
-            <EquipmentCell/>
+      {weapon.map((item: any) => (
+            <EquipmentCell key={item.id} path={item.img} type='weapon'/>
         ))}
+        {armor.map((item: any) => (
+            <EquipmentCell key={item.id} path={item.img} type='armor'/>
+        ))}
+        
+        
     </div>
   );
 }
--- src/components/Equipment/EquipmentCell.tsx
+++ src/components/Equipment/EquipmentCell.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Equipment.css'
 
+interface Props {
+  path: any
+  type: string
+}
 
-export const EquipmentCell = () => {
-
+export const EquipmentCell = (props:Props) => {
+// policzyć ile elementów i dodać tyle żeby było 30
+//baza danych do przechowywaia pozycji , wyrzuconych oczek i kolejności
 
   return (
     <div className='ceil'>
-      
+      <img src={'./images/'+props.type+ '/' + props.path + '.webp'} alt="" />
     </div>
   );
 }
