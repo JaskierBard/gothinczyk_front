56864045a91f66b7649d10827faaa38d173ecd8d
JaskierBared <mateuszswiderski009@gmail.com>
2023-07-31 20:31:11 +0200

    created code window

---
+++ src/components/CodesBar/CodesBar.tsx
@@ -0,0 +1,16 @@
+import React, { useState }  from "react";
+import "./CodesBar.css";
+
+export const Codes = () => {
+
+
+
+  return (
+      <div className="Codes">
+      <img className="code_flame" src="./flame_1.png" alt="" />
+      <img className="code_text" src="./gothic.png" alt="" />
+
+        
+      </div>
+  );
+};
