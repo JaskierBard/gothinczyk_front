17d70f40395fdb4526e96edd4c58157307d8f2e4
JaskierBared <mateuszswiderski009@gmail.com>
2023-08-03 15:29:36 +0200

    added fog component

---
+++ src/components/common/Smoke/Smoke.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import './Smoke.css'
+
+export const Smoke = () => {
+
+
+
+
+    return (    
+      <>
+      <div className="anim-body">
+    <div className="fog-anim">
+
+    </div>
+
+   </div>
+   </>
+    )
+  
+  
+    
+  }
