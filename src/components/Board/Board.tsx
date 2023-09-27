db07f5f6bc434b501ad741cc815b02ae6e954f68
JaskierBared <mateuszswiderski009@gmail.com>
2023-09-27 19:07:26 +0200

    enemy view in board

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -20,15 +20,30 @@ export const Background = () => {
     <>
       <section className="background">
         <div className="bgc">
-          {/* <div className="space" style={{top:'7.6vh', left:'7.6vh'}}></div>
-          <div className="space" style={{top:'7.6vh', right:'7.6vh'}}></div>
-          <div className="space" style={{bottom:'7.6vh', left:'7.6vh'}}></div>
-          <div className="space" style={{bottom:'7.6vh', right:'7.6vh'}}></div> */}
 
 {boardFinal ? (
   boardFinal.map((item: any, index: any) => {
-    console.log(index);
-    return <div key={index} className={item.type} style={{gridArea:`${'a' +item.lp}`}}></div>;
+    if (item.enemyImg !== '') {
+      console.log(item.enemyImg)
+      const style = {
+        gridArea: `a${item.lp}`,
+        backgroundImage: `url("./enemy/${item.enemyImg}.webp")`
+      };
+      return <div key={index} className='base' style={style}></div>;
+    } else if (item.base === 'grass'){
+      const style = {
+        gridArea: `a${item.lp}`,
+        backgroundImage: `url("./images/plates/grass.webp")`
+      };
+      return <div key={index} className='base' style={style}></div>;
+    } else {
+      const style = {
+        gridArea: `a${item.lp}`,
+      };
+      return <div key={index} className='base' style={style}></div>;
+
+    }
+    
   })
 ) : (
   <p>Tablica boardFinal nie istnieje lub jest pusta</p>
